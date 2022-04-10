4b4c15b314cd726b620d34b0536e131f4c0319b1
ricardojikal0311 <15992876+ricardojikal0311@users.noreply.github.com>
2022-04-10 05:25:53 +0000

    inAppChat attachments changes

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2023-02-01"  # YYYY-MM-DD
-END_DATE="2023-10-18"    # YYYY-MM-DD
+START_DATE="2022-04-01"  # YYYY-MM-DD
+END_DATE="2022-12-18"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
