a72fb98621391f80cfc27ffb408a5d53e03b406d
ricardojikal0311 <15992876+ricardojikal0311@users.noreply.github.com>
2023-02-02 15:29:53 +0000

    Fix Custom Notification Channel

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2024-02-01"  # YYYY-MM-DD
-END_DATE="2024-07-18"    # YYYY-MM-DD
+START_DATE="2023-02-01"  # YYYY-MM-DD
+END_DATE="2023-10-18"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
